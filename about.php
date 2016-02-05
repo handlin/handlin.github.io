d1129b529e6c789ca6406632281b5814bb3fdb5e
mzuri <handlin@uchicago.edu>
2016-02-04 19:02:39 -0600

    php includes work! at least through MAMP

---
+++ about.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <?php include("head.html") ?>
+</head>
+
+<body>
+<div id="layout">
+
+    <!-- MAIN MENU -->
+    <div id="menu-home">
+        <?php include("main_menu.html") ?>
+    </div>
+    
+    <div id="main">
+ 
+        <!--HEADER-->
+        <div class="header">
+            <!--<h1 id="project_title">Mzuri S. Handlin</h1>-->
+            <h1>About</h1>
+        </div>
+
+        <!-- MAIN CONTENT -->
+        <div class="content">
+            <h2 class="content-subhead">Programming experience 1</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <h2 class="content-subhead">Programming experience 2</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+
+        </div>
+
+        <!--FOOTER-->
+        <?php include("footer.html") ?>
+
+    </div>
+</div>
+
+<script src="scripts/ui.js"></script>
+
+</body>
+</html>
